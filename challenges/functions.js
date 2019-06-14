3e9d3b2db3747b685be9aa9a0a54699f43741aba
Lisa <lmkirk@gmail.com>
2019-06-14 12:36:33 -0400

    add, multiple, greeting created.  Stuck on consume().  Will come back to it

--- challenges/functions.js
+++ challenges/functions.js
@@ -16,20 +16,27 @@ console.log(aHigherOrderFunction(10, 15));
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function consume(param1, param2, cb) {
+}
+
 function add(num1, num2, cb) {
   return cb = (num1 + num2);
 }
 
 function multiply(num1, num2, cb) {
   return cb = (num1 * num2);
+}
 
+function greeting(firstName, lastName, cb) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
 }
 
 
+
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
